2463349e3ad904fb2aef3ad471dff6a695a195e0
false <false>
2026-05-06 18:09:30 +0530

    fix: try fixing systemdservices

--- modules/daemons.nix
+++ modules/daemons.nix
@@ -2,38 +2,34 @@
   pkgs, inputs, ...
 }:
 {
-
   systemd.user.services.anyrun-daemon = {
     Unit = {
-      Description = "Anyrun Daemon";
-      # Ensures it only starts once Wayland is ready
-      After = [ "graphical-session.target" ];
-      PartOf = [ "graphical-session.target" ];
+      description = "Anyrun Daemon";
+      after = [ "graphical-session.target" ];
+      partOf = [ "graphical-session.target" ];
     };
     Service = {
-      # Environment ensures it can find its own plugins and Wayland socket
-      Environment = [ "PATH=${pkgs.anyrun}/bin" ];
-      ExecStart = "${pkgs.anyrun}/bin/anyrun daemon";
-      Restart = "on-failure";
+      environment = { PATH = "${pkgs.anyrun}/bin"; };
+      execStart = "${pkgs.anyrun}/bin/anyrun daemon";
+      restart = "on-failure";
     };
     Install = {
-      WantedBy = [ "graphical-session.target" ];
+      wantedBy = [ "graphical-session.target" ];
     };
   };
 
   systemd.user.services.kidex = {
     Unit = {
-      Description = "Kidex file indexer";
-      After = [ "graphical-session-pre.target" ];
-      Partof = [ "graphical-session.target" ];
+      description = "Kidex file indexer";
+      after = [ "graphical-session-pre.target" ];
+      partOf = [ "graphical-session.target" ];
     };
     Service = {
-      ExecStart = "${inputs.kidex.packages.${pkgs.stdenv.hostPlatform.system}.kidex}/bin/kidex";
-      Restart = "always";
+      execStart = "${inputs.kidex.packages.${pkgs.stdenv.hostPlatform.system}.kidex}/bin/kidex";
+      restart = "always";
     };
     Install = {
-      WantedBy = [ "default.target" ];
+      wantedBy = [ "default.target" ];
     };
   };
 }
-
--- modules/packages/productivity.nix
+++ modules/packages/productivity.nix
@@ -5,7 +5,7 @@
 {
   environment.systemPackages = with pkgs; [
     anki
-    onlyoffice
+    onlyoffice-desktopeditors
     drawing
     obsidian
   ];
