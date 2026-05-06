e8dd345452dceca59cd69809227e7a43712023be
false <false>
2026-05-06 18:12:43 +0530

    fix: systemd service files

--- modules/daemons.nix
+++ modules/daemons.nix
@@ -3,33 +3,27 @@
 }:
 {
   systemd.user.services.anyrun-daemon = {
-    Unit = {
-      description = "Anyrun Daemon";
-      after = [ "graphical-session.target" ];
-      partOf = [ "graphical-session.target" ];
-    };
-    Service = {
-      environment = { PATH = "${pkgs.anyrun}/bin"; };
-      execStart = "${pkgs.anyrun}/bin/anyrun daemon";
-      restart = "on-failure";
-    };
-    Install = {
-      wantedBy = [ "graphical-session.target" ];
+    enable = true;
+    description = "Anyrun Daemon";
+    after = [ "graphical-session.target" ];
+    partOf = [ "graphical-session.target" ];
+    wantedBy = [ "graphical-session.target" ];
+    serviceConfig = {
+      Environment = "PATH=${pkgs.anyrun}/bin";
+      ExecStart = "${pkgs.anyrun}/bin/anyrun daemon";
+      Restart = "on-failure";
     };
   };
 
   systemd.user.services.kidex = {
-    Unit = {
-      description = "Kidex file indexer";
-      after = [ "graphical-session-pre.target" ];
-      partOf = [ "graphical-session.target" ];
-    };
-    Service = {
-      execStart = "${inputs.kidex.packages.${pkgs.stdenv.hostPlatform.system}.kidex}/bin/kidex";
-      restart = "always";
-    };
-    Install = {
-      wantedBy = [ "default.target" ];
+    enable = true;
+    description = "Kidex file indexer";
+    after = [ "graphical-session-pre.target" ];
+    partOf = [ "graphical-session.target" ];
+    wantedBy = [ "default.target" ];
+    serviceConfig = {
+      ExecStart = "${inputs.kidex.packages.${pkgs.stdenv.hostPlatform.system}.kidex}/bin/kidex";
+      Restart = "always";
     };
   };
 }
