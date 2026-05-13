af291aefee92d4a040c5bcee93e0fe0dfc71bd40
bhaswata <bhaswata08@gmail.com>
2026-05-13 19:23:34 +0530

    feat: add essential nvidia graphics card and coretools

--- modules/boot.nix
+++ modules/boot.nix
@@ -4,9 +4,8 @@
 }:
 
 {
-
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.kernelPackages = pkgs.linuxPackages_latest;
-
+  boot.kernelParams = [ "nvidia-drm.modeset=1" ];
 }
